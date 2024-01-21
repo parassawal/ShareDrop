fedf8cb1d05963b47655cd1ff147fbc1065d9d0b
parassawal <parassawal2810@gmail.com>
2024-01-21 21:27:20 +0530

    first commit

--- public/script.js
+++ public/script.js
@@ -1,20 +1,27 @@
 function uploadFile() {
-    const fileInput = document.getElementById('fileInput');
-    const file = fileInput.files[0];
-  
-    const formData = new FormData();
-    formData.append('file', file);
-  
-    fetch('/upload', {
-      method: 'POST',
-      body: formData,
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log('File uploaded:', data.filename);
-    })
-    .catch(error => {
-      console.error('Error uploading file:', error);
-    });
+  const fileInput = document.getElementById('fileInput');
+  const file = fileInput.files[0];
+
+  if (!file) {
+    alert("Please choose a file before uploading.");
+    return;
   }
-  
+
+  const formData = new FormData();
+  formData.append('file', file);
+
+  fetch('/upload', {
+    method: 'POST',
+    body: formData,
+  })
+  .then(response => response.json())
+  .then(data => {
+    const statusElement = document.getElementById('status');
+    statusElement.innerHTML = `File uploaded: ${data.filename}`;
+  })
+  .catch(error => {
+    console.error('Error uploading file:', error);
+    const statusElement = document.getElementById('status');
+    statusElement.innerHTML = 'Error uploading file. Please try again.';
+  });
+}
